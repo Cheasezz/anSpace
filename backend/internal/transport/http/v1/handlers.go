4ff0bcc58f90171932a2cc9375d9306ed4357310
Cheasezz <yuradolgo@gmail.com>
2024-08-04 03:54:03 +0300

    delete coments

--- backend/internal/transport/http/v1/handlers.go
+++ backend/internal/transport/http/v1/handlers.go
@@ -30,22 +30,6 @@ func (h *Handlers) InitRoutes(router *gin.RouterGroup) {
 	v1 := router.Group("/v1")
 	{
 		h.initAuthRoutes(v1)
-		// wel := v1.Group("/wel", h.userIdentity)
-		// {
-		// 	wel.GET("/come", h.Welcome)
-		// }
 	}
 }
 
-// func (h *Handlers) Welcome(c *gin.Context) {
-// 	id, err := getUserId(c)
-// 	if err != nil {
-// 		newErrorResponse(c, h.log, http.StatusInternalServerError, err)
-// 		return
-// 	}
-
-// 	logrus.Printf("hello Its Welcome Func!!! And userID is: %s", id)
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"cho": "Norm vse",
-// 	})
-// }
