95326d3c688faca713b0695f864c7461e1d07ef1
Cheasezz <yuradolgo@gmail.com>
2024-11-13 14:29:30 +0300

    fix cookie domain

--- backend/internal/transport/http/v1/response.go
+++ backend/internal/transport/http/v1/response.go
@@ -30,7 +30,7 @@ func newErrorResponse(c *gin.Context, l logger.Logger, statusCode int, err error
 }
 
 func newTokenResponse(c *gin.Context, t auth.Tokens, cfg config.HTTP) {
-	c.SetCookie(rtCookieName, t.Refresh.Token, t.Refresh.TTLInSec, "/", cfg.Host, false, true)
+	c.SetCookie(rtCookieName, t.Refresh.Token, t.Refresh.TTLInSec, "/", cfg.FrontendOrigins[0], false, true)
 	c.JSON(http.StatusOK, tokenResponse{
 		Access: t.Access,
 	})
